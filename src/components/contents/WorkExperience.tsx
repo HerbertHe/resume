8dee353e82d700f5d60262f08d8e621924807f65
Herbert <herbert.he0229@gmail.com>
2022-08-01 10:51:05 +0800

    feat :sparkles: : Update

--- src/components/contents/WorkExperience.tsx
+++ src/components/contents/WorkExperience.tsx
@@ -4,8 +4,9 @@ import ExperienceBox from "../boxs/ExperienceBox"
 const WorkExperience = () => {
     return <ContentBox title="WorkExperience">
         <ul>
-            <ExperienceBox name="Party and Government Official" location="Queren Township People's Government" address="Mori, Changji, Xinjiang" time="Aug. 2021 ~" />
-            <ExperienceBox name="Collegiate Volunteer to CFWP" location="China's Far West Program (Service in Xinjiang)" address="Mori, Changji, Xinjiang" time="Aug. 2021 ~" />
+            <ExperienceBox name="Clerk" location="Mori Kazakh Autonomous County Statistics Bureau" address="Mori, Changji, Xinjiang" time="Jul. 2022 ~" />
+            <ExperienceBox name="Party and Government Official" location="Queren Township People's Government" address="Mori, Changji, Xinjiang" time="Aug. 2021 ~ Jul. 2022" />
+            <ExperienceBox name="Collegiate Volunteer to CFWP" location="China's Far West Program" address="Mori, Changji, Xinjiang" time="Aug. 2021 ~" />
         </ul>
     </ContentBox>
 }
