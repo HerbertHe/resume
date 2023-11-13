029ece48bf413ff99a852ffa103e95ff8db7f5f5
HerbertHe <Herbert.He0229@gmail.com>
2023-11-13 13:40:22 +0800

    feat :sparkles: : update sponsor

--- src/components/contents/Banner.tsx
+++ src/components/contents/Banner.tsx
@@ -6,14 +6,25 @@ interface IBanner {
 const banners: IBanner[] = [
     { title: "Blog", url: "https://ibert.me" },
     { title: "Idea", url: "https://idea.ibert.me" },
+    { title: "Sponsor", url: "https://sponsor.ibert.me" },
     { title: "Liandi", url: "https://ld246.com/member/HerbertHe" },
 ]
 
-const Banner = () => <ul className="w-full flex flex-row justify-start items-center">
-    {banners.map(({ title, url }, idx) => <li key={url} className="font-light">
-        <a href={url} title={title} className="m-2 underline underline-dotted hover:(underline-solid)">{title}</a>
-        { idx !== banners.length - 1 && <span className="m-2">·</span> }
-    </li>)}
-</ul>
+const Banner = () => (
+    <ul className="w-full flex flex-row justify-start items-center">
+        {banners.map(({ title, url }, idx) => (
+            <li key={url} className="font-light">
+                <a
+                    href={url}
+                    title={title}
+                    className="m-2 underline underline-dotted hover:(underline-solid)"
+                >
+                    {title}
+                </a>
+                {idx !== banners.length - 1 && <span className="m-2">·</span>}
+            </li>
+        ))}
+    </ul>
+)
 
 export default Banner
